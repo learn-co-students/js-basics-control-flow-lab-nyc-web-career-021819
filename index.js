b0e9b02bf2865a2f293230ad8d7c045a9403ad3a
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-03-31 12:36:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  let output
+  if (someValue <= 400) {
+    output = 'This one is on me!'
+  }
+  else if (1999 < someValue && someValue < 2500) {
+    output = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    output = 'No can do.'
+  }
+  return output
+}
+
+function ternaryCheckCity(city) {
+  let output
+  if (city === 'NYC') {
+    output = 'Ok, sounds good.'
+  }
+  else if (city ==='Pittsburgh') {
+    output = 'No go.'
+
+  }
+  return output
+}
+
+function switchOnCharmFromTip(tip) {
+  let output
+  if (tip === 'generous') {
+    output = 'Thank you so much.'
+  }
+  else if (tip ==='not as generous') {
+    output = 'Thank you.'
+  }
+  else if (tip === 'thanks for everything'){
+    output = 'Bye.'
+  }
+  return output
+
+}
